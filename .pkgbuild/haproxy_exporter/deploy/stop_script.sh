bdab4e7f9f601cb05a127645b8db0abe95b2f24f
liuwenxin <a1072865261@163.com>
2021-10-10 18:50:18 +0800

    create pkgbuild file

---
+++ .pkgbuild/haproxy_exporter/deploy/stop_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Name    : stop_script.py
+# Date    : 2016.03.28
+# Func    : 停止脚本
+# Note    : 注意：当前路径为应用部署文件夹
+
+#############################################################
+# 用户自定义
+process_name="haproxy_exporter"       # 进程名
+
+ps -ef | grep ${process_name} | awk '{print $2}' | xargs kill 2>/dev/null
+
+exit 0
